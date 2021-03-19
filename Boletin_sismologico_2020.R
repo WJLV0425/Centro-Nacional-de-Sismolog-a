f64e40625d4e30ad8d139a552d6254c9c6f4ff6e
WJLV0425 <weliferlv@hotmail.com>
2021-03-19 18:46:53 -0400

    Docs_base

--- Boletin_sismologico_2020.R
+++ Boletin_sismologico_2020.R
@@ -1,8 +1,26 @@
-###Boletin sismologico 2020
+### Boletin sismologico 2020
 
 ## Autor: WJLV0425
 ## Fecha: 19/03/21
 
-#Convertir archivo sismos2020.txt a matriz.
+# Mandato
+
+# Crear una base de datos que contenga las provincias por separado
+# Hacer un mapa de densidad sísmica del 2019 por provincias
+# Establece correlación entre la cantidad de sismos por provincias y la densidad de fallas geológicas. 
+
+
+  
+# Convertir archivo .txt a CSV: Abrir libro de excel -> ir a "Datos" (barra superior) -> Obtener datos externos -> abrir el archivo .txt -> guardar como archivo CSV.
+
+#Abrir archivo CSV.
+  #Actualizar paquetes para importar datos CSV: 'readr', 'Rcpp'.
+
+install.packages("readr", "Rcpp")
+library(readr)
+Sismos2020 <- read_csv("Sismos2020.csv")
+View(Sismos2020)
+
+
 
 
